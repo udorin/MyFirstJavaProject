3ec348f0ceb3bb8757989f968f66bbf5bf695a0c
Dorin <dorinurs@gmail.com>
2019-02-08 12:04:45 +0200

    HomeWork Java 1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,32 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //Homework Java 1.
 
-        System.out.println("Hello There");
+        //Exercitiul 1 .
+        System.out.println("Hello");
+        System.out.println("Dorin");
+
+        //exercitiul 2
+        System.out.println("The addition of 7 and 8 is : " + (7 +8));
+
+        //exercitiul 3
+
+        int x = 10;
+        int y = 2;
+        int divide = x / y;
+        System.out.println(divide);
+        //sau
+        System.out.println(10 / 2);
+
+        //exercitiul 4.
+        int a = -5 + 8 * 6;
+        System.out.println("The resultat from a is : " + a);
+        int b = (55 + 9) % 9;
+        System.out.println("The resultat from b is : " + b);
+        int c = 20 + ((-3 * 5) / 8);
+        System.out.println("The resultat from c is : "+ c);
+        int d = (5 + ((15 / 3) * 2)) - (8 % 3);
+        System.out.println("The resultat from d is " + d);
     }
 }
