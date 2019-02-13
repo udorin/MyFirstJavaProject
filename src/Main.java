42406fb903cc02f48b5c7069fe989b21fee0292b
Dorin <dorinurs@gmail.com>
2019-02-13 19:56:26 +0200

    Java 2 Clasa

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         //Homework Java 1.
 
         //Exercitiul 1 .
+       /*
         System.out.println("Hello");
         System.out.println("Dorin");
+
 
         //exercitiul 2
         System.out.println("The addition of 7 and 8 is : " + (7 +8));
@@ -31,5 +35,37 @@ public class Main {
         System.out.println("The resultat from cfinal  is : "+ cfinal);
         int d = (5 + ((15 / 3) * 2)) - (8 % 3);
         System.out.println("The resultat from d is " + d);
+        */
+
+
+        int resultatScadere = scadere(5, 7);
+        System.out.println(resultatScadere);
+
+        int rezInmultire = inmultire(4, 6);
+        System.out.println(rezInmultire);
+        double rezImpartire = impartire(11, 5);
+        System.out.println(rezImpartire);
+
+
+
+
+
     }
+
+    public static int scadere(int primulNumar, int alDoileaNumar) {
+        int scadere1 = primulNumar - alDoileaNumar;
+        return scadere1;
+
+    }
+
+    public static int inmultire(int pN, int aN) {
+        int rezultatInmultire = pN * aN;
+        return rezultatInmultire;
+    }
+
+    public static double impartire(double impartire1, double impartire2) {
+        double rezultateImpartire = impartire1 / impartire2;
+        return rezultateImpartire;
+    }
+
 }
